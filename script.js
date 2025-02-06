d2088223ff1dfaf2473b944eb84ad2af1400e62d
Ritesh Kumar <7976ritesh@gmail.com>
2025-02-06 13:37:35 +0530

    first

---
+++ script.js
@@ -0,0 +1,27 @@
+function addNewWeField(){
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-1")
+    newNode.setAttribute("rows",2);
+    newNode.setAttribute("placeholder","Enter here")
+
+    let weOb=document.getElementById("we");
+    let weAddButtonOb=document.getElementById("weAddButton");
+
+    weOb.insertBefore(newNode,weAddButtonOb);
+}
+
+function addNewAqField(){
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("aqField");
+    newNode.classList.add("mt-1")
+    newNode.setAttribute("rows",2);
+    newNode.setAttribute("placeholder","Enter here")
+
+    let aqOb=document.getElementById("aq");
+    let aqAddButtonOb=document.getElementById("aqAddButton");
+
+    aqOb.insertBefore(newNode,aqAddButtonOb);
+}
