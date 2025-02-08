6cc5ff389101326140b11784aa0b63c16f9add61
Ritesh Kumar <7976ritesh@gmail.com>
2025-02-09 00:34:09 +0530

    updated templete

--- script.js
+++ script.js
@@ -1,82 +1,109 @@
-function addNewWeField(){
-    let newNode = document.createElement("textarea");
-    newNode.classList.add("form-control");
-    newNode.classList.add("weField");
-    newNode.classList.add("mt-1")
-    newNode.setAttribute("rows",2);
-    newNode.setAttribute("placeholder","Enter here")
+function addNewWeField() {
+  let newNode = document.createElement("textarea");
+  newNode.classList.add("form-control");
+  newNode.classList.add("weField");
+  newNode.classList.add("mt-1");
+  newNode.setAttribute("rows", 2);
+  newNode.setAttribute("placeholder", "Enter here");
 
-    let weOb=document.getElementById("we");
-    let weAddButtonOb=document.getElementById("weAddButton");
+  let weOb = document.getElementById("we");
+  let weAddButtonOb = document.getElementById("weAddButton");
 
-    weOb.insertBefore(newNode,weAddButtonOb);
+  weOb.insertBefore(newNode, weAddButtonOb);
 }
 
-function addNewAqField(){
-    let newNode = document.createElement("textarea");
-    newNode.classList.add("form-control");
-    newNode.classList.add("aqField");
-    newNode.classList.add("mt-1")
-    newNode.setAttribute("rows",2);
-    newNode.setAttribute("placeholder","Enter here")
+function addNewProField() {
+  let newNode = document.createElement("textarea");
+  newNode.classList.add("form-control");
+  newNode.classList.add("proField");
+  newNode.classList.add("mt-1");
+  newNode.setAttribute("rows", 2);
+  newNode.setAttribute("placeholder", "Enter here");
 
-    let aqOb=document.getElementById("aq");
-    let aqAddButtonOb=document.getElementById("aqAddButton");
+  let proOb = document.getElementById("pro");
+  let proAddButtonOb = document.getElementById("proAddButton");
 
-    aqOb.insertBefore(newNode,aqAddButtonOb);
+  proOb.insertBefore(newNode, proAddButtonOb);
 }
 
+function addNewAqField() {
+  let newNode = document.createElement("textarea");
+  newNode.classList.add("form-control");
+  newNode.classList.add("aqField");
+  newNode.classList.add("mt-1");
+  newNode.setAttribute("rows", 2);
+  newNode.setAttribute("placeholder", "Enter here");
 
-function generateCv(){
-    //  console.log("generation cv")
-    let nameField=document.getElementById("nameField").value;
-    let nameT=document.getElementById("nameT1");
+  let aqOb = document.getElementById("aq");
+  let aqAddButtonOb = document.getElementById("aqAddButton");
 
-    nameT.innerHTML=nameField;
-    // setting the nameT2 value directly
-    document.getElementById("nameT2").innerHTML=nameField
-    // setting the contact value directly
-    document.getElementById("contactT").innerHTML=document.getElementById("contactField").value;
-    // address
-    document.getElementById("addressT").innerHTML=document.getElementById("addressField").value;
+  aqOb.insertBefore(newNode, aqAddButtonOb);
+}
 
-    // links 
-    document.getElementById("linkedinT").innerHTML=document.getElementById("linkedin").value;
-    document.getElementById("githubT").innerHTML=document.getElementById("github").value;
-    document.getElementById("instagramT").innerHTML=document.getElementById("instagram").value;
+function generateCv() {
+  let nameField = document.getElementById("nameField").value;
+  document.getElementById("nameT1").innerHTML = nameField;
+  // document.getElementById("nameT2").innerHTML=nameField;
+  document.getElementById("contactT").innerHTML =
+    document.getElementById("contactField").value;
+  document.getElementById("addressT").innerHTML =
+    document.getElementById("addressField").value;
+  document.getElementById("linkedinT").innerHTML =
+    document.getElementById("linkedin").value;
+  document.getElementById("githubT").innerHTML =
+    document.getElementById("github").value;
+  document.getElementById("instagramT").innerHTML =
+    document.getElementById("instagram").value;
+  document.getElementById("objectiveT").innerHTML =
+    document.getElementById("objectiveField").value;
 
-    // objective
-    document.getElementById("objectiveT").innerHTML=document.getElementById("objectiveField").value;
-
-    // work experience
-    
-    let wes = document.getElementsByClassName("weField");
-    let str=""
-    for(let e of wes){
-        str=str+`<li>${e.value}</li>`;
+  let wes = document.getElementsByClassName("weField");
+  let str = "";
+  for (let e of wes) {
+    if (e.value.trim() !== "") {
+      str += `<li>${e.value}</li>`;
     }
-    document.getElementById("weT").innerHTML=str;
+  }
+  if (str === "") {
+    document.getElementById("we-card").style.display = "none";
+  } else {
+    document.getElementById("we-card").style.display = "block";
 
-    //Education Qualification
+    document.getElementById("weT").innerHTML = str;
+  }
 
-    let aqs = document.getElementsByClassName("aqField");
-    let str2=""
-    for(let el of aqs){
-        str2=str2+`<li>${el.value}</li>`;
+
+
+  let pros = document.getElementsByClassName("proField");
+  let str1 = "";
+  for (let e of pros) {
+    if (e.value.trim() !== "") {
+      str1 += `<li>${e.value}</li>`;
     }
-    document.getElementById("aqT").innerHTML=str2;
+  }
+  if (str1 === "") {
+    document.getElementById("pro-card").style.display = "none";
+  } else {
+    document.getElementById("pro-card").style.display = "block";
+
+    document.getElementById("proT").innerHTML = str1;
+  }
 
 
-    //hiding and showing the form and the templete respectivley
 
-    document.getElementById("cv-form").style.display="none";
-    document.getElementById("cv-templete").style.display="block"; 
 
+  let aqs = document.getElementsByClassName("aqField");
+  let str2 = "";
+  for (let e of aqs) {
+    str2 += `<li>${e.value}</li>`;
+  }
+  document.getElementById("aqT").innerHTML = str2;
+
+  document.getElementById("cv-form").style.display = "none";
+  document.getElementById("cv-templete").style.display = "block";
 }
 
-
-// print cv
-
-function printCv(){
-    window.print();
+function printCv() {
+  const element = document.getElementById("cv-templete");
+  html2pdf().from(element).save("resume.pdf");
 }
